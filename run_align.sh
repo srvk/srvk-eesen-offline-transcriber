b1ead81ab133468b7f18b86d49151dbac9756f63
Eric Riebling <er1k@cs.cmu.edu>
2016-09-30 15:13:59 +0000

    support vagrant ssh -c

--- run_align.sh
+++ run_align.sh
@@ -9,10 +9,14 @@
 #   scripts/parse_cha_xml.py
 #   scripts/merge_align_cha.py
 
+BASEDIR=$(dirname $0)
+
 filename=$(basename "$1")
 basename="${filename%.*}"
 dirname=$(dirname "$1")
 extension="${filename##*.}"
+
+cd $BASEDIR
 
 ./align.sh $1
 python scripts/merge_align_cha.py $dirname/$basename.xml build/output/$basename.ali >build/trans/$basename/$basename.xml
@@ -20,4 +24,10 @@ python scripts/merge_align_cha.py $dirname/$basename.xml build/output/$basename.
 # convert back to CHA format
 
  ~/bin/lib/zulu8.17.0.3-jdk8.0.102-linux_x64/bin/java -cp lib/chatter.jar org.talkbank.chatter.App -inputFormat xml -outputFormat cha -output build/output/$basename.cha build/trans/$basename/$basename.xml
+
+# copy intermediate files to output folder
+
+cp $dirname/$basename.stm build/output
+mv $dirname/$basename.xml build/output
+
 
