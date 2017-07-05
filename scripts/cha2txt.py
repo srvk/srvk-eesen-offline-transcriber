415c2bb2ae08533bb2165135fce9418d38b74ea0
riebling <er1k@cs.cmu.edu>
2017-07-05 17:09:08 -0400

    Add explanatory comment

--- scripts/cha2txt.py
+++ scripts/cha2txt.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+
+# For historical purposes (should really be called cha2stm): a first cut
+# at a simplistic way to convert CHA to STM format.  In practice this gets
+# a lot of things wrong, and cha2stm.sh should be used instead. But this does
+# illustrate some of the basics of where to find time markers in CHA files
+# and how to print output that looks like STM
 import sys,subprocess
 
 filename = sys.argv[1].replace(".cha","")
